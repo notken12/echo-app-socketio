b14440ff56ebe7a032adec12c46f1190a392537d
notken12 <kendotzhou@gmail.com>
2021-12-06 21:37:59 -0500

    load balancing works, fix socket

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 // Setup basic express server
 var express = require('express');
 var app = express();
@@ -7,7 +9,7 @@ var redis = require('socket.io-redis');
 var port = process.env.PORT || 3000;
 var serverName = process.env.NAME || 'Unknown';
 
-io.adapter(redis({ host: 'redis', port: 6379 }));
+io.adapter(redis({ host: '127.0.0.1', port: 6379 }));
 
 server.listen(port, function () {
     console.log('Server listening at port %d', port);
@@ -22,11 +24,16 @@ app.head('/health', function (req, res) {
     res.sendStatus(200);
 });
 
+app.get('/name', function (req, res) {
+    res.send(serverName);
+});
+
 // echo socket.io message
 
 io.on('connection', function (socket) {
     // tell client the server they're connected to
     socket.emit('server name', serverName);
+    socket.emit('server port', port);
     
     socket.on('send message', function (msg) {
         console.log('message: ' + msg + ' received by ' + serverName);
--- public/main.js
+++ public/main.js
@@ -5,15 +5,19 @@ var messagesEl = document.getElementById('messages');
 var messageInputEl = document.getElementById('message-input');
 var messageFormEl = document.getElementById('message-form');
 
-socket.on('server name', function(name) {
+socket.on('server name', function (name) {
     serverNameEl.innerHTML = "Connected to " + name;
+});
+
+socket.on('server port', function (port) {
+    serverNameEl.innerHTML += "\nConnected to " + port;
 });
 
 socket.on('echo message', function (msg) {
     messagesEl.innerHTML += '<p>' + msg + '</p>';
 });
 
-messageFormEl.addEventListener('submit', function(e) {
+messageFormEl.addEventListener('submit', function (e) {
     e.preventDefault();
     socket.emit('send message', messageInputEl.value);
     messageInputEl.value = '';
